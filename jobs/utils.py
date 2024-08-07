c584ef1412507c747e9078d136bd14ddece84299
EAGLEEARTH <ufukcicek199@gmail.com>
2024-08-08 00:38:25 +0300

    push

--- jobs/utils.py
+++ jobs/utils.py
@@ -15,7 +15,6 @@ from django.contrib.auth.models import User
 from profiles.models import GeneratedCV
 from translateContext.translateContext import *
 
-
 EMAIL_HOST_USER = "info@cvbuilder.tech"
 
 def check_generated_cv():
@@ -25,11 +24,17 @@ def check_generated_cv():
 
 def send_generated_reminder_email(user):
     current_lang = Profile.objects.filter(user_id=user.id).first()
-    mail_context = get_trans_lang_1(current_lang.language)
+    if current_lang and current_lang.language:
+        current_lang = current_lang.language
+    else:
+        current_lang = "en"  
+
+    mail_context = get_trans_lang_1(current_lang)
     subject = mail_context.get("profile_update_mail_title")
     email_temp = mail_context.get('profile_update_mail_content')
     email_content = email_temp.format(username=user.username)
     mail_context["profile_update_mail_content"] = email_content
+    print(mail_context.get('profile_update_mail_content'))
     email_content = render_to_string('email_templates/profileUpdate.html', mail_context)
     email = EmailMessage(subject, email_content, EMAIL_HOST_USER, to=[user.email])
     email.content_subtype = 'html'
@@ -45,7 +50,12 @@ def update_cv():
 
 def send_update_reminder_email(user):
     current_lang = Profile.objects.filter(user_id=user.id).first()
-    mail_context = get_trans_lang_1(current_lang.language)
+    if current_lang and current_lang.language:
+        current_lang = current_lang.language
+    else:
+        current_lang = "en"  
+
+    mail_context = get_trans_lang_1(current_lang)
     subject = mail_context.get("profile_update_mail_title")
     email_temp = mail_context.get('profile_update_mail_content1')
     email_content = email_temp.format(username=user.username)
