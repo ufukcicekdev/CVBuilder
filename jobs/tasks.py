856cd0ea7881b3272ce9409eab9e2618b9e59127
EAGLEEARTH <ufukcicek199@gmail.com>
2024-08-08 20:46:05 +0300

    push

--- jobs/tasks.py
+++ jobs/tasks.py
@@ -5,9 +5,8 @@ from apscheduler.schedulers.background import BackgroundScheduler
 def start():
 
     scheduler = BackgroundScheduler()
-    scheduler.add_job(check_generated_cv, 'cron', day_of_week='mon', hour=9)
-
-    scheduler.add_job(update_cv, 'cron', day_of_week='mon', hour=9)
+    scheduler.add_job(check_generated_cv, 'cron', day_of_week='mon,fri', hour=9)
+    scheduler.add_job(update_cv, 'cron', day_of_week='mon,fri', hour=9)
 
 
     #scheduler.add_job(check_generated_cv, 'interval', seconds=10)
